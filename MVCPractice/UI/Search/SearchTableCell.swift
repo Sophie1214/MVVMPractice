175a3f586687f3e029022a3ae3329b0ff475d748
Sophie Su <ssu@trakitwireless.com>
2020-03-06 15:43:33 -0500

    first commit

---
+++ MVCPractice/UI/Search/SearchTableCell.swift
@@ -0,0 +1,19 @@
+//
+//  SearchTableCell.swift
+//  MVCPractice
+//
+//  Created by Sophie Su on 2020-03-06.
+//  Copyright © 2020 Trak iT. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableCell: UITableViewCell {
+
+ 
+    override init(style: UITableViewCell.Style, reuseIdentifier: String){
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        
+    }
+}
---
+++ MVCPractice/UI/Search/SearchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MVCPractice
+//
+//  Created by Sophie Su on 2020-03-06.
+//  Copyright © 2020 Trak iT. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
